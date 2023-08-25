5d071fb9449204e28ee7fff633b0607fd1a56d81
vandevan <vandevanpassos123@gmail.com>
2023-08-25 19:32:01 -0300

    Pendetes e Home

--- Banco de Dados/Banco_de_dados.sql
+++ Banco de Dados/Banco_de_dados.sql
@@ -1,9 +1,7 @@
 -- Criação da tabela de usuários
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    senha VARCHAR(100) NOT NULL
+
 );
 
 -- Inserção de dados de exemplo
