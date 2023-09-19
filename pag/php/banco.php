311a8ff9e826f90a2e489ffa70b98a8c288aa3ee
nick <fafaniconi@gmail.com>
2023-09-19 16:53:34 -0300

    codigo barra

---
+++ pag/php/banco.php
@@ -0,0 +1,32 @@
+<?php
+    class Banco{
+        private $conn;//representa um canal de comunicação com a base de dados
+        private $serverName = "localhost"; //nome do servidor ip
+        private $userName = "root"; //nome do usuario
+        private $password = ""; 
+        private $dbname = ""; //base de dados que será utilizada
+
+        function conectar(){
+            try{
+                $this->conn = new PDO("mysql:host=$this->serverName;dbname=$this->dbname", $this->userName, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                #echo "Conexão realizada";
+                return $this->conn; 
+            }catch(PDOException $e){
+                echo "Erro ao estabelecer conexão: ".$e->getMessage(); 
+            }
+        }
+
+        function fecharConexao(){
+            try{
+                $this->conn = null; 
+                echo "Conexão finalizada.";
+            }catch(PDOException $e){
+                echo "Erro ao finalizar conexão.".$e;
+            }
+        }
+    }
+
+    #$bd = new Banco();
+    #$bd->conectar();
+?>
