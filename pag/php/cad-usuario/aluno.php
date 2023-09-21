a0e06bde181b3b9d8d3a3c683a5ed1ee8ffb6fce
nick <fafaniconi@gmail.com>
2023-09-21 10:43:31 -0300

    inclusao do sql

---
+++ pag/php/cad-usuario/aluno.php
@@ -0,0 +1,75 @@
+<?php
+include_once '../banco.php';
+
+class Aluno{
+    public $id_aluno;
+    public $nome;
+    public $curso;
+    public $turma;
+    public $matricula;
+    public $email;
+    public $senha;
+
+    function __construct($nome, $curso, $turma, $matricula, $email, $senha){
+        $this->nome = $nome;
+        $this->turma =  $turma;
+        $this->curso = $curso;
+        $this->matricula = $matricula;
+        $this->email = $email;
+        $this->senha = $senha;
+    }
+
+    function inserir(){
+        $banco = new Banco();
+        $conn = $banco->conectar();
+        try{
+            $stmt = $conn->prepare("insert into aluno (nome, curso, turma, matricula, email, senha) values(:nome, :curso, :turma, 
+            :matricula, :email, :senha)");
+            $stmt->bindParam(':nome',$this->nome);
+            $stmt->bindParam(':telefone',$this->curso);
+            $stmt->bindParam(':turma',$this->turma);
+            $stmt->bindParam(':matricula',$this->matricula);
+            $stmt->bindParam(':email',$this->email);
+            $stmt->bindParam(':senha',$this->senha);
+            $stmt->execute();
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+        $banco->fecharConexao();
+    }
+    function getIdAluno(){
+        return $this->id_aluno;
+    }
+
+    function setIdAluno($id_aluno){
+        $this->id_aluno = $id_aluno;
+    }
+    static function carregar($id_aluno){
+        try{
+            $banco = new Banco();
+            $conn = $banco->conectar();
+            $stmt = $conn->prepare("select * from aluno where id_aluno = :id_aluno");
+            $stmt->bindParam(':id_aluno',$id_aluno);
+            $stmt->execute();
+            $aluno = null;
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            foreach($stmt->fetchAll() as $v => $value){
+                $aluno = new Aluno($value['nome'],
+                $value['telefone'],
+                $value['email'],$value['id_curso'],
+                $value['nascimento'],
+                $value['sexo']);
+                $aluno->setIdAluno( $value['id_aluno']);
+             }
+            return $aluno;
+
+        }catch(PDOException $e){
+            echo "Erro " . $e->getMessage();
+        }
+    }
+
+
+}
+
+
+?>
---
+++ pag/php/cad-usuario/alunoHelper.php
@@ -0,0 +1,73 @@
+<?php
+    include_once '../cad-usuario/aluno.php';
+    include_once '../banco.php';
+
+   if(isset($_POST['tipo'])){
+        $tipo = $_POST['tipo'];
+        if($tipo === 'cad_aluno'){
+            cadastrarAluno();
+            header('Location:home_aluno.html');
+        }/*else if($tipo === 'excluir_aluno'){
+            excluir_aluno();
+            header('Location:index.php');
+        }else if($tipo === 'editar_aluno'){
+            editar_aluno();
+            header('Location:index.php');
+        }*/
+   }
+
+   /*function  editar_aluno(){
+        $aluno = Aluno::carregar($_POST['id_aluno']);
+        $aluno->nome = $_POST['nome'];
+        $aluno->telefone = $_POST['telefone'];
+        $aluno->email = $_POST['email'];
+       // $curso = $_POST['curso'];
+       $aluno->nascimento = $_POST['nascimento'];
+       $aluno->sexo = $_POST['sexo'];
+       $aluno->editar();
+   }*/
+
+    function cadastrarAluno(){
+      //  echo "oi";
+        $nome = $_POST['nome'];
+        $curso = $_POST['curso'];
+        $turma = $_POST['turma'];
+        $matricula = $_POST['matricula'];
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+
+        $aluno = new Aluno($nome,$curso,$turma,$matricula,
+        $email,$senha);
+        $aluno->inserir();
+
+    }
+
+    function getAlunos(){
+        try{
+            $banco = new Banco();
+            $conn = $banco->conectar();
+            $stmt = $conn->prepare("select * from aluno");
+            $stmt->execute();
+           // $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $alunos = array();
+            foreach($stmt->fetchAll() as $v => $value){
+                $aluno = new Aluno($value['nome'], $value['curso'], $value['email'], $value['id_curso'], $value['email'],
+                $value['senha']);
+                $aluno->setIdAluno( $value['id_aluno']);
+                array_push($alunos,$aluno);
+            }
+
+            //var_dump($alunos);
+            return $alunos;
+
+        }catch(PDOException $e){
+            echo "Erro " . $e->getMessage();
+        }
+    }
+
+   // getAlunos();
+
+/*    $aluno = new Aluno('Pedro Carvalho','75 99147 8160',
+    'pedro@gmail.com',2,'01-08-2014','Masculino');
+    $aluno->inserir();*/
+?>
