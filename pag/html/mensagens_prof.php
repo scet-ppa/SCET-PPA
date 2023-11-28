2f6c65f549bb46a9bc98c6ca3ffb2a242cb5b7a0
Guilherme <guilhermedario2005@gmail.com>
2023-11-28 07:02:18 -0300

    correções

--- pag/html/mensagens_prof.php
+++ pag/html/mensagens_prof.php
@@ -39,7 +39,7 @@ if(isset($_POST['send'])){
     <title>Mensagens - Professor</title>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQ3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="../css/mensagens_coord.css">
+    <link rel="stylesheet" type="text/css" href="../css/mensagens_prof.css">
     <link rel="shortcut icon" type="imagex/png" href="../img/logo-scet.jpg">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/allencasul/lonica@d9dbccfa5b0a4666760e4f72b28effa775c56857/css/cdn/lonica.css" integrity="sha256-E1S8yAbnRZ6uM4sA6NMSgTyoDsdK1ZCjBYF3lqXqv6Q=" crossorigin="anonymous">
 
@@ -56,37 +56,43 @@ if(isset($_POST['send'])){
                     </a>
                 </li>
                 <li class="list">
-                    <a href="../html/home_prof.php">
-                        <span class="icon"><ion-icon name="notifications-outline"></ion-icon></span>
+                    <a href="../html/home_coord.php">
+                        <span class="icon"><ion-icon name="home"></ion-icon></span>
                         <span class="list">Home</span>
                     </a>
                 </li>
                 <li class="list">
-                    <a href="../html/mensagens_prof.php">
+                    <a href="../html/mensagens_coord.php">
                         <span class="icon"><ion-icon name="chatbubble-outline"></ion-icon></span>
                         <span class="list">Mensagens</span>
                     </a>
                 </li>
                 <li class="list">
-                    <a href="../html/notificacoes_prof.php">
+                    <a href="../html/notificacoes_coord.php">
                         <span class="icon"><ion-icon name="notifications-outline"></ion-icon></span>
                         <span class="list">Notificações</span>
                     </a>
                 </li>
                 <li class="list">
-                    <a href="../html/estagio_prof.php">
+                    <a href="../html/prof-coord.php">
+                        <span class="icon"><ion-icon name="school-outline"></ion-icon></span>
+                        <span class="list">Professores</span>
+                    </a>
+                </li>
+                <li class="list">
+                    <a href="../html/estagio_coord.php">
                         <span class="icon"><ion-icon name="briefcase-outline"></ion-icon></span>
                         <span class="list">Estágio</span>
                     </a>
                 </li>
                 <li class="list">
-                    <a href="../html/prof-tcc.php">
+                    <a href="../html/coor-tcc.php">
                         <span class="icon"><ion-icon name="newspaper-outline"></ion-icon></span>
                         <span class="list">TCC</span>
                     </a>
                 </li>
                 <li class="list">
-                    <a href="../html/pendente_prof.php">
+                    <a href="../html/pendente_coord.php">
                         <span class="icon"><ion-icon name="alert-circle-outline"></ion-icon></span>
                         <span class="list">Pendentes</span>
                     </a>
@@ -100,7 +106,36 @@ if(isset($_POST['send'])){
             </ul>
         </div>
     </div>
+   
 
-    
+    <div class="container">
+        <form class="display-grid row-gap-1-rem" method="post">
+            <input class="box-shadow-primary" name="name" type="text" placeholder="Name" autocomplete="off" required />
+            <input class="box-shadow-primary" name="email" type="email" placeholder="Email" autocomplete="off" required />
+            <input class="box-shadow-primary" name="subject" type="text" placeholder="Subject" autocomplete="off" required />
+            <textarea class="box-shadow-primary" name="message" placeholder="Message..." required></textarea>
+            <button type="submit" name="send">
+              Send <i class="fa-solid fa-paper-plane color-white margin-left-1-rem"></i>
+            </button>
+          </form>
+    </div>
+   
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+    <script>
+        const list = document.querySelectorAll('.list');
+        function activeLink(){
+            list.forEach((item) =>
+            item.classList.remove('active'));
+            this.classList.add('active'); 
+        }
+            list.forEach((item) =>
+            item.addEventListener('click', activeLink)); 
+            src="https://kit.fontawesome.com/1e8d61f212.js"
+            
+    </script>
+   
+
 </body>
 </html>
